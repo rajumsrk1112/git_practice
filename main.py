fd4e9acea298dd9bafdd2f89cce665309cceb49b
Sai_Manthina <msrkraju222@gmail.com>
2022-07-17 01:08:41 +0530

    main.py changes in rebase branch

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 print("Hello World")
 
 from modules.utils import add_two_numbers
+from modules.utils import mul_two_numbers
+
 
 print(f"Addition of two numbers: {add_two_numbers(100,25)}")
+
+
+print(f"Multiplication of two numbers: {mul_two_numbers(10,20)}")
+
